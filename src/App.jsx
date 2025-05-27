1704f44588d6ea294345e529c44f259e9996170e
Mehedi Mehedi <mehedi25122003@gmail.com>
2025-05-27 13:54:41 +0600

    break

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
+import "./App.css";
+import Home from "./Components/Home";
+import murder from './Images/murder.gif'
+import joker from './Images/joker.gif'
+import eyes from './Images/scaryEyes.gif'
+import deadbody from './Images/bodyfall.gif'
+import movie from './Images/girlwatchingmovie.gif'
+import scarry from './Images/realscary.gif'
+import real from './Images/realbody.png'
 
-import './App.css'
-import Home from './Components/Home'
 
 function App() {
-
-
   return (
-    <div className='w-11/12 mx-auto grid grid-cols-12'>
-      
-      <div className='col-span-10'>
-<Home></Home>
+    <div className="w-11/12 mx-auto grid grid-cols-12 gap-2">
+      <div className="col-span-10">
+        <Home></Home>
       </div>
-      <div className='col-span-2'>
-
+      <div className="col-span-2 grid grid-cols-1">
+          <img className="border-2 border-gray-400 p-2" src={eyes} alt="eyesphoto" />
+          <img className="border-2 border-gray-400 p-2" src={real} alt="eyesphoto" />
+          <img className="border-2 border-gray-400 p-2" src={scarry} alt="eyesphoto" />
+          <img className="border-2 border-gray-400 p-2" src={joker} alt="eyesphoto" />
+          
+          <img className="border-2 border-gray-400 p-2" src={movie} alt="eyesphoto" />
+          <img className="border-2 border-gray-400 p-2" src={murder} alt="eyesphoto" />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,27 +1,77 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to={'/'}>Home</NavLink>
+      </li>
+      
+      <li>
+        <NavLink to={'/guns'}>Guns</NavLink>
+      </li>
+     
+      <li>
+        <NavLink to={'/drugs'}>Drugs</NavLink>
+      </li>
+    
+      <li>
+        <NavLink to={'/girls'}>Girls</NavLink>
+      </li>
+     
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+    
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+     
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+   
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+  
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+    </>
+  );
   return (
     <section>
       <div className="flex justify-between">
         <div className="join text-3xl font-boldHack mt-1 font-extrabold">
-        <span className="text-red-600 font-extrabold join-item">g</span>
-        <span className="join-item font-bold">Modex</span>
+          <span className="text-red-600 font-extrabold join-item">g</span>
+          <span className="join-item font-bold">Modex</span>
+        </div>
+        <div>
+          <ul
+            className="flex
+             gap-2 font-semibold text-gray-400"
+          >
+            <li>
+              <a href="">My Account</a>
+            </li>
+            |
+            <li>
+              <a href="">Help</a>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
-        <ul
-          className="flex
-             gap-2 font-semibold text-gray-400"
-        >
-          <li>
-            <a href="">My Account</a>
-          </li>
-          |
-          <li>
-            <a href="">Help</a>
-          </li>
-        </ul>
-      </div>
+        <div className=" bg-base-100 shadow-sm">
+          <div className=" hidden lg:flex justify-around w-full items-center bg-gray-400">
+            <ul className="menu menu-horizontal lg:text-2xl font-bold px-1 flex justify-between items-center w-full bg-black text-white ">
+              {links}
+            </ul>
+          </div>
+          
+        </div>
       </div>
     </section>
   );
